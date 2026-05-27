a7191bf4db88d7d043ce726ef7f2f177038910ac
2025050222 <gun4143@naver.com>
2026-05-27 15:35:38 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    // 10글자 + 널 문자를 위해 크기를 11로 설정
-    char x[11]; 
 
-    char max_char;
-    int max_count = 0;   // 전체에서 가장 많이 나온 횟수
     
+    // 최대 10글자 + 문자열 끝을 나타내는 \0 저장 공간
+    char x[11];
+
+    // 가장 많이 나온 문자를 저장할 변수
+    char max_char;
+    // 현재 까지의 최대 등장 횟수 저장
+    int max_count = 0;
+
     // 문자열 입력 받기
     scanf("%s", x);
 
-    
+    // 문자열의 각 문자를 하나씩 검사
+    // p는 현재 검사 중인 문자를 가리키는 포인터
     for (char *p = x; p < x + 10; p++) {
-        int now_count = 0; // 새로운 문자를 검사할 때마다 0으로 초기화
 
-        for (char *q = x; q < x + 10; q++) {
-            if (*p == *q) {
+        // 현재 문자의 등장 횟수 저장 변수
+        int now_count = 0;
+        // q를 이용해 문자열 전체를 다시 탐색
+        for (char *q =x;q < x +10; q++) {
+
+            // p가 가리키는 문자와 q가 가리키는 문자가 같으면
+            if (*p ==*q) {
+
+            //  횟수 증가
                 now_count++;
             }
         }
 
-        // 루프가 끝나면 현재 문자의 빈도수가 역대 최댓값보다 클때 갱신
+        // 현재 문자의 등장 횟수가 지금까지의 최대 등장 횟수보다 크면 변경
         if (now_count > max_count) {
+            // 최대 등장 횟수 저장
             max_count = now_count;
-            max_char = *p; // 현재 가리키는 문자를 저장
+
+            // 가장 많이 나온 문자 저장
+            max_char = *p;
         }
     }
 
-    // 결과 출력
+    // 가장 많이 나온 문자와 등장 횟수 출력
     printf("%c %d\n", max_char, max_count);
-    
+
     return 0;
 }
